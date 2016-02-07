69b5e95efa145507ea10b9238b6cb6815fbc0487
Eduardo Quiroz <eduardo_quiroz@icloud.com>
2016-02-06 23:59:32 -0500

    valida 3

--- pruebaGit/ViewController.h
+++ pruebaGit/ViewController.h
@@ -4,7 +4,7 @@
 //
 //  Created by Jessica y Eduardo Quiroz on 31/1/16.
 //  Copyright © 2016 Jessica y Eduardo Quiroz. All rights reserved.
-//
+//  
 
 #import <UIKit/UIKit.h>
 
--- pruebaGit/ViewController.m
+++ pruebaGit/ViewController.m
@@ -4,7 +4,7 @@
 //
 //  Created by Jessica y Eduardo Quiroz on 31/1/16.
 //  Copyright © 2016 Jessica y Eduardo Quiroz. All rights reserved.
-//
+//  modificacion 3
 
 #import "ViewController.h"
 
@@ -18,7 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
-
+    // modificacion 3
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
